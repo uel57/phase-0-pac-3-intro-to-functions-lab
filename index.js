a41c1ba3df2e51635a5957bf9913ca63684756e3
samk <samuelkariuki1@student.moringaschool.com>
2023-06-12 13:47:01 +0300

    incomplete

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(hello){
+    return "hello".toUpperCase();
+}
+console.log("hello".toUpperCase())
+
+function whisper(HELLO){
+    return "HELLO".toLowerCase();
+}
+
+console.log("HELLO".toLowerCase())
+
+    
+
+function logWhisper(hello){
+console.log("hello".toUpperCase())
+}
